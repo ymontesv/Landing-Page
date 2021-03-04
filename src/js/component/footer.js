210b7d10dad48a4a35bdb5a04f7d74b5232ec365
ymontesv <yarmain.m@gmail.com>
2021-03-04 02:44:23 +0000

    Landing Page

---
+++ src/js/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer className="bg-dark text-center text-lg-start">
+			<div className="text-center p-3">
+				<a className="text-primary" href="https://mdbootstrap.com/">
+					ymontesv.com
+				</a>
+			</div>
+		</footer>
+	);
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,6 +5,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
 import { Cards } from "./cards";
+import { Footer } from "./footer";
 
 //create your first component
 export function Home() {
@@ -12,7 +13,13 @@ export function Home() {
 		<div className="text-center mt-5">
 			<Navbar />
 			<Jumbotron />
-			<Cards />
+			<div className="d-flex justify-content-center">
+				<Cards />
+				<Cards />
+				<Cards />
+				<Cards />
+			</div>
+			<Footer />
 		</div>
 	);
 }
